86c1e094823df8199edfbcd9baeddc88f07e2244
Phoenix <x.rhythm@qq.com>
2024-11-08 20:47:36 +0800

    feat: update error for CodingKey

--- Sources/ReerCodableMacros/CodingKeyImpl.swift
+++ Sources/ReerCodableMacros/CodingKeyImpl.swift
@@ -15,6 +15,30 @@ public struct CodingKey: PeerMacro {
         providingPeersOf declaration: some DeclSyntaxProtocol,
         in context: some MacroExpansionContext
     ) throws -> [DeclSyntax] {
+        guard
+            let variable = declaration.as(VariableDeclSyntax.self),
+            let name = variable.name
+        else {
+            return []
+        }
+        
+        let codingKey = variable.attributes.first(where: {
+            let attribute = $0.as(AttributeSyntax.self)?
+                .attributeName.as(IdentifierTypeSyntax.self)?
+                .trimmedDescription
+            return attribute == "CodingKey"
+        })
+        if let codingKey {
+            let inputKeys = codingKey.as(AttributeSyntax.self)?
+                .arguments?.as(LabeledExprListSyntax.self)?
+                .compactMap { $0.expression.trimmedDescription } ?? []
+            if inputKeys.isEmpty {
+                throw MacroError(text: "Property `\(name)` requires at least one coding key.")
+            }
+            try inputKeys.forEach {
+                if $0 == "\"\"" { throw MacroError(text: "Empty coding key detected of property `\(name)`.") }
+            }
+        }
         return []
     }
 }
