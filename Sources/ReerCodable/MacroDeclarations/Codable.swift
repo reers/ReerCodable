877983c4ff39c46319165c24d5a23bb38fb478ed
phoenix <x.rhythm@qq.com>
2024-12-04 02:33:44 +0800

    refactor: split macros to files

---
+++ Sources/ReerCodable/MacroDeclarations/Codable.swift
@@ -0,0 +1,27 @@
+//
+//  Copyright © 2024 reers.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+@attached(extension, conformances: Codable, ReerCodableDelegate, names: arbitrary)
+@attached(member, names: named(init(from:)), named(encode(to:)), arbitrary)
+public macro Codable(memberwiseInit: Bool = true) = #externalMacro(module: "ReerCodableMacros", type: "Codable")
+
+@attached(member, names: named(init(from:)), named(encode(to:)), arbitrary)
+public macro InheritedCodable() = #externalMacro(module: "ReerCodableMacros", type: "InheritedCodable")
