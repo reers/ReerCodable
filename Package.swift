87ced913c387d2c225958da07346e348adeeeaa7
Phoenix <x.rhythm@qq.com>
2026-01-23 13:24:54 +0800

    feat: auto flexible type with trait (#35)
    * feat: auto flexible type by trait
    
    * Update documents

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,19 +15,21 @@ let package = Package(
         .visionOS(.v1)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ReerCodable",
             targets: ["ReerCodable"]
         )
     ],
+    traits: [
+        .trait(
+            name: "AutoFlexibleType",
+            description: "Enable automatic type conversion for all @Codable/@Decodable types"
+        ),
+    ],
     dependencies: [
         .package(url: "https://github.com/swiftlang/swift-syntax", from: "600.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        // Macro implementation that performs the source transformation of a macro.
         .macro(
             name: "ReerCodableMacros",
             dependencies: [
@@ -35,11 +37,7 @@ let package = Package(
                 .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
             ]
         ),
-
-        // Library that exposes a macro as part of its API, which is used in client programs.
         .target(name: "ReerCodable", dependencies: ["ReerCodableMacros"]),
-
-        // A test target used to develop the macro implementation.
         .testTarget(
             name: "ReerCodableTests",
             dependencies: [
