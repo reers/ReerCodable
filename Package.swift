1e8f6c79dd25d14d5bd731b141a5589b51db6961
phoenix <x.rhythm@qq.com>
2026-07-25 15:24:58 +0800

    Widen swift-syntax dependency to a compatible version range.

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax", from: "603.0.0"),
+        .package(url: "https://github.com/swiftlang/swift-syntax", "603.0.0"..<"606.0.0"),
     ],
     targets: [
         .macro(
